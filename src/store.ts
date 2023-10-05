8a878e6feca53ff51076acdf2e5834bca44a7c0d
Laurent Mbuyu <LMbuyu@osiristrading.com>
2023-10-05 12:27:58 +0200

    [New]: Chosen place property has been added to the store.

--- src/store.ts
+++ src/store.ts
@@ -4,19 +4,22 @@ type State = {
     name: string
     place: [] | any[]
     event: [] | any
+    chosenPlace: string
 }
 
 type Action = {
     setName: (name: State['name']) => void
     setPlace: (place: State['place']) => void
-    setEvents: (event: State['event']) => void
+    setEvent: (event: State['event']) => void
 }
 
 export const useStore = create<State & Action>((set) => ({
     name: "",
     place: [],
     event: [],
+    chosenPlace: "",
     setName: (name) => set(() => ({ name: name })),
     setPlace: (place: any) => set({ place: place }),
-    setEvents: (event: any) => set({ event: event }),
+    setEvent: (event: any) => set({ event: event }),
+    setChosenPlace: (chosenPlace: any) => set({ chosenPlace: chosenPlace })
 }));
