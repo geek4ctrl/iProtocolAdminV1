bec3b60317258e1f332376ab1417dda0ffbd6c76
Laurent Mbuyu <LMbuyu@osiristrading.com>
2023-10-05 12:36:28 +0200

    [New]: Properties are passed to navigation client component.

--- components/UnauthenticatedUser.tsx
+++ components/UnauthenticatedUser.tsx
@@ -1,10 +1,10 @@
 import { NavigationClientComponent } from "./NavigationClientComponent";
 import UnauthenticatedUserEventsComponent from "./UnauthenticatedUserEventsComponent";
 
-export default function UnauthenticatedUser({ allEventsToDisplay }: { allEventsToDisplay: any }) {
+export default function UnauthenticatedUser({ allEventsToDisplay, allGomaPlaces, allKinshasaPlaces }: { allEventsToDisplay: any, allGomaPlaces: any, allKinshasaPlaces: any }) {
     return (
         <div className="w-full flex flex-col items-center">
-            <NavigationClientComponent />
+            <NavigationClientComponent allGomaPlaces={allGomaPlaces} allKinshasaPlaces={allKinshasaPlaces} />
             <UnauthenticatedUserEventsComponent allEventsToDisplay={allEventsToDisplay} />
         </div>
     )
