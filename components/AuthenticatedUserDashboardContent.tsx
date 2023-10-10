ae928ebf7fe8a02ffc994efd15ebf348309542ee
Laurent Mbuyu <LMbuyu@osiristrading.com>
2023-10-10 14:10:08 +0200

    [New]: Toast messages have been added when selecting the dashboard option.

--- components/AuthenticatedUserDashboardContent.tsx
+++ components/AuthenticatedUserDashboardContent.tsx
@@ -2,10 +2,24 @@
 
 import { useStore } from "@/src/store";
 import Link from "next/link";
-
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useRouter } from 'next/navigation';
 
 export default function AuthenticatedUserDashboardContent({ plans }: { plans: any }) {
+    const router = useRouter()
+
+
+    function callAllFunctions(reservationType: any) {
+
+        notify(reservationType);
+        chooseReservationType(reservationType);
+
+        setTimeout(() => {
+            routeToEventPage()
+        }, 3000)
+
+    }
 
     function chooseReservationType(reservationType: any) {
         useStore.setState((state) => ({
@@ -13,8 +27,15 @@ export default function AuthenticatedUserDashboardContent({ plans }: { plans: an
         }))
     }
 
+    const notify = ((reservationType: any) => toast(`You have chosen ${reservationType}`));
+
+    const routeToEventPage = () => {
+        router.push('/events', { scroll: false })
+    }
+
     return (
-        <div className='mt-16 space-y-6 justify-center gap-6 sm:grid sm:grid-cols-2 sm:space-y-0 lg:grid-cols-2' style={{ display: "flex" }}>
+        <div className='mt-16 space-y-6 justify-center gap-6 sm:grid sm:grid-cols-2 sm:space-y-0 lg:grid-cols-2' style={{ display: "flex", flexDirection: "column" }}>
+            <ToastContainer />
             {
                 plans.map((item: any, idx: any) => (
                     <div key={idx} className='relative flex-1 flex items-stretch flex-col p-8 rounded-xl border-2' style={{ backgroundImage: `url(${item.image})`, width: "10rem", padding: "2rem", textAlign: "center", borderRadius: "15px", marginTop: "5rem" }}>
@@ -25,12 +46,12 @@ export default function AuthenticatedUserDashboardContent({ plans }: { plans: an
                         </div>
                         <ul className='py-8 space-y-3'>
                         </ul>
-                        <div className="flex-1 flex items-end">
-                            <Link href="/events" className="w-full" onClick={() => chooseReservationType(item.name)}>
-                                <button className='px-3 py-3 rounded-lg w-full font-semibold text-sm duration-150 text-white bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700' style={{ cursor: "pointer" }} >
+                        <div className="flex-1 flex items-end" >
+                            <div className="w-full" >
+                                <button onClick={() => callAllFunctions(item.name)} className='px-3 py-3 rounded-lg w-full font-semibold text-sm duration-150 text-white bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700' style={{ cursor: "pointer" }} >
                                     Choose
                                 </button>
-                            </Link>
+                            </div>
                         </div>
                     </div>
                 ))
